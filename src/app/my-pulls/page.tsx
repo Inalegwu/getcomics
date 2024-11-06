b222a29687363fb639ea9aad789c1280ad570d4d
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-11-06 15:24:25 +0000

    more ui work

--- src/app/my-pulls/page.tsx
+++ src/app/my-pulls/page.tsx
@@ -1,7 +1,43 @@
-import { Flex } from '@radix-ui/themes';
+import dc from '@/app/assets/images/dc.png';
+import marvel from '@/app/assets/images/marvel.png';
+import { Flex, Text } from '@radix-ui/themes';
+import Image from 'next/image';
 
 export default function Page() {
   return (
-    <Flex className="w-full h-screen bg-zinc-100 py-4 px-3">my pulls</Flex>
+    <Flex
+      direction="column"
+      className="w-full h-screen bg-zinc-100 py-[3.6rem]"
+      gap="3"
+    >
+      <Flex
+        className="px-3 py-3 w-full bg-slate-50/30 backdrop-blur-lg fixed border-y-solid border-y-[1px] border-y-zinc-200"
+        align="center"
+        justify="between"
+      >
+        <Flex align="end" justify="start" gap="3">
+          <Text weight="bold" size="3">
+            Publishers:{' '}
+          </Text>
+          <button className="px-7 py-1 shadow-sm rounded-md bg-zinc-100 hover:bg-zinc-400/5 border-zinc-200 border-[1px] border-solid">
+            <Flex width="100%" align="center" justify="start" gap="2">
+              <Image alt="dc-logo" width={16} height={16} src={dc} />
+              <Text>DC</Text>
+            </Flex>
+          </button>
+          <button className="px-7 py-1 shadow-sm rounded-md bg-zinc-100 hover:bg-zinc-400/5 border-zinc-200 border-[1px] border-solid">
+            <Flex width="100%" align="center" justify="start" gap="2">
+              <Image alt="dc-logo" width={16} height={16} src={marvel} />
+              <Text>Marvel</Text>
+            </Flex>
+          </button>
+        </Flex>
+      </Flex>
+      <Flex className="px-3 py-[5rem]">
+        <Text size="8" weight="bold">
+          My Pulls
+        </Text>
+      </Flex>
+    </Flex>
   );
 }
